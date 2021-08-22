79e9a956c6a7985550145903b436abf937d14ea8
khoubaib <khoubaib.bnz@gmail.com>
2021-08-22 08:48:41 +0100

    lara-s

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CarController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('cars', CarController::class);
+Route::get('/cars/search/{model}', [CarController::class, 'search']);
+
+//Route::get('/cars', [CarController::class, 'index']);
+//Route::post('/cars', [CarController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
